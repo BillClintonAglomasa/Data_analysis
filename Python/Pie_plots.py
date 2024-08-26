522d98cb3bcbbfd3b89691784e36297f7c9de375
https://github.com/BillClintonAglomasa <aglomasa@yahoo.com>
2024-08-26 19:21:03 +0000

    Placed space before and after the '=' operator

--- Python/Pie_plots.py
+++ Python/Pie_plots.py
@@ -44,9 +44,9 @@ color_map = {"Hypertension": "rgb(255, 127, 14)",
 
 # Donut plot for gender 
 # Initialize the subplot figure with two rows and one column
-gender_pie = make_subplots(rows=2, cols=1, 
+gender_pie = make_subplots(rows = 2, cols = 1, 
                            specs=[[{"type": "pie"}], [{"type": "pie"}]], 
-                           vertical_spacing=0.3
+                           vertical_spacing = 0.3
 )
 
 # Loop through each column and create a pie chart
@@ -58,32 +58,32 @@ for i, column in enumerate(value_columns_gender):
     colors = [color_map[label] for label in df_filtered["Data element"]]
 
     # Add the pie chart to the correct row (1 for Female, 2 for Male)
-    gender_pie.add_trace(go.Pie(labels=df_filtered["Data element"], 
-                                values=df_filtered[column], 
-                                hole=0.55, 
-                                textinfo="label+percent", 
-                                insidetextorientation="radial", 
-                                marker=dict(colors=colors)
+    gender_pie.add_trace(go.Pie(labels = df_filtered["Data element"], 
+                                values = df_filtered[column], 
+                                hole = 0.55, 
+                                textinfo = "label+percent", 
+                                insidetextorientation = "radial", 
+                                marker = dict(colors = colors)
                          ), 
-                         row=i+1, col=1
+                         row = i+1, col = 1
     )
 
 # Update layout with annotations
 gender_pie.update_layout(
-    annotations=[
-        dict(text="<b>Female Diagnosis Patterns in Top 10 Diseases<b>", 
-            x=0.5, y=1.11, font=dict(size=16), showarrow=False
+    annotations = [
+        dict(text = "<b>Female Diagnosis Patterns in Top 10 Diseases<b>", 
+            x = 0.5, y = 1.11, font = dict(size = 16), showarrow = False
         ),
-        dict(text="<b>Male Diagnosis Patterns in Top 10 Diseases<b>", 
-            x=0.5, y=0.43, font=dict(size=16), showarrow=False
+        dict(text = "<b>Male Diagnosis Patterns in Top 10 Diseases<b>", 
+            x = 0.5, y = 0.43, font = dict(size = 16), showarrow = False
         ),
-        dict(text="Female", x=0.5, y=0.86, font=dict(size=20, color="black"), showarrow=False
+        dict(text = "Female", x = 0.5, y = 0.86, font = dict(size = 20, color = "black"), showarrow = False
         ),
-        dict(text="Male", x=0.5, y=0.15, font=dict(size=20, color="black"), showarrow=False
+        dict(text = "Male", x = 0.5, y = 0.15, font = dict(size = 20, color = "black"), showarrow = False
         )
     ],
-    showlegend=False,
-    height=600  # Adjust height for better visibility
+    showlegend = False,
+    height = 600  # Adjust height for better visibility
 )
 
 # Show the pie chart
@@ -92,9 +92,9 @@ gender_pie.write_image("Gender_pie_chart.png")
 
 # Separated pie chart for age group
 # Initialize the subplot figure with two rows and one column
-age_pie = make_subplots(rows=2, cols=1, 
-                           specs=[[{"type": "pie"}], [{"type": "pie"}]], 
-                           vertical_spacing=0.3
+age_pie = make_subplots(rows = 2, cols = 1, 
+                           specs = [[{"type": "pie"}], [{"type": "pie"}]], 
+                           vertical_spacing = 0.3
 )
 
 # Loop through each column and create a pie chart
@@ -106,47 +106,47 @@ for i, column in enumerate(value_columns_age):
     colors = [color_map[label] for label in df_filtered["Data element"]]
 
     # Add the pie chart to the correct row (1 for Female, 2 for Male)
-    age_pie.add_trace(go.Pie(labels=df_filtered["Data element"], 
-                             values=df_filtered[column],  
-                             textinfo="label+percent", 
-                             insidetextorientation="radial", 
-                             marker=dict(colors=colors)
+    age_pie.add_trace(go.Pie(labels = df_filtered["Data element"], 
+                             values = df_filtered[column],  
+                             textinfo = "label+percent", 
+                             insidetextorientation = "radial", 
+                             marker = dict(colors=colors)
                      ), 
-                     row=i+1, col=1
+                     row = i+1, col = 1
     )
 
 # Update layout with annotations
 age_pie.update_layout(
-    annotations=[
-        dict(text=f"<b>Most Affected Age Group ({age_group_highest}): Disease Distribution<b>", 
-            x=0.5, y=1.11, font=dict(size=16), showarrow=False
+    annotations = [
+        dict(text = f"<b>Most Affected Age Group ({age_group_highest}): Disease Distribution<b>", 
+            x = 0.5, y = 1.11, font = dict(size = 16), showarrow = False
         ),
-        dict(text=f"<b>Least Affected Age Group ({age_group_lowest}): Disease Distribution<b>", 
-            x=0.5, y=0.43, font=dict(size=16), showarrow=False
+        dict(text = f"<b>Least Affected Age Group ({age_group_lowest}): Disease Distribution<b>", 
+            x = 0.5, y = 0.43, font = dict(size = 16), showarrow = False
         )
     ],
-    showlegend=False,
-    height=600  # Adjust height for better visibility
+    showlegend = False,
+    height = 600  # Adjust height for better visibility
 )
 
 # Customize the appearance of the slices
-age_pie.update_traces(textinfo='percent+label', pull=[0.8, 0.6, 0.4, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+age_pie.update_traces(textinfo = "percent+label", pull = [0.8, 0.6, 0.4, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2])
 age_pie.write_image("Age_pie_chart.png")
 
 #the bars are too thick, the background should be changed to white. the lines should be changed to blue
 # You need to create a unique axis for the plot
 # Bar chart for 
 # Create the bar plot
-top_10 = go.Figure(data=[
+top_10 = go.Figure(data = [
     go.Bar(x = data["Data element"], y = data["Total"])
 ])
 
 # Customize the layout
 top_10.update_layout(
-    title='Top 10 Most Diagnosed Disease',
-   # xaxis_title='Categories',
-    #yaxis_title='Values',
-    #template='plotly_dark'  # Optional: 'plotly', 'ggplot2', 'seaborn', etc.
+    title="Top 10 Most Diagnosed Disease",
+   # xaxis_title = 'Categories',
+    #yaxis_title = 'Values',
+    template = "ggplot2"
 )
 
 # Show the plot
